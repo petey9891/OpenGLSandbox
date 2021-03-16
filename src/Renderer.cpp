ea02ec77739461b41ab754c48853095fa25e7c7d
Andrew Peterson <16apeterson@gmail.com>
2021-03-16 01:02:25 -0500

    abstracted opengl shader code

---
+++ src/Renderer.cpp
@@ -0,0 +1,14 @@
+#include "Renderer.h"
+#include <iostream>
+
+void Renderer::clear() const {
+    GLCall(glClear(GL_COLOR_BUFFER_BIT));
+}
+
+void Renderer::draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const {
+    shader.bind();
+    va.bind();
+    ib.bind();
+
+    GLCall(glDrawElements(GL_TRIANGLES, ib.getCount(), GL_UNSIGNED_INT, nullptr));
+}
---
+++ src/Renderer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <GL/glew.h>
+
+#include "Debug.h"
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
+class Renderer {
+    public:
+        void clear() const;
+        void draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
